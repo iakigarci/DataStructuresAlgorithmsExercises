f3001e6e77a597b56ec1b34b454a64bf60e3c4a3
Iñaki Garcia <garcianoya@hotmail.es>
2018-11-25 14:46:29 +0100

    Planteado el ejercicio Clinica

--- src/juegodebloques/Bloque.java
+++ src/juegodebloques/Bloque.java
@@ -5,4 +5,10 @@ public class Bloque {
 	int puntos;
 	int salto;
 	String direccion;
+
+	public Bloque(int pP, int pS, String pD) {
+		this.puntos = pP;
+		this.salto = pS;
+		this.direccion = pD;
+	}
 }
--- src/juegodebloques/Juego.java
+++ src/juegodebloques/Juego.java
@@ -12,15 +12,41 @@ public class Juego {
 
 		for (int i = 0; i<=NUMCOLAS-1; i++) {
 			tablero[i] = new Stack<Bloque>();
-	 }
+	    }
 	 // código para “llenar” aleatoriamente las pilas de “bloques”
 	}
 	public int jugar() {
-		System.out.println();
+		int pelota = 0;
+		int puntos = 0;
+        if (tablero.length % 2 == 0) {
+            pelota = tablero.length / 2;
+        }else{
+            pelota = (tablero.length / 2)+1;
+        }
+        boolean salir = false;
+        Bloque bloque = tablero[pelota].pop();
+        puntos = puntos + bloque.puntos;
+        while (!salir ){
+            if (bloque.direccion.equals("derecha")) {
+                pelota = pelota+bloque.salto;
+            }else{
+                pelota = pelota-bloque.salto;
+            }
 
+            if (pelota>tablero.length || pelota<tablero.length){
+                salir = true;
+                puntos = 0;
+                System.out.println("LA PELOTA SE HA SALIDO");
+            }else if (tablero[pelota].isEmpty()){
+                salir = true;
+                System.out.println("TERMINADO");
+            }else {
+                bloque = tablero[pelota].pop();
+                puntos = puntos + bloque.puntos;
+            }
 
-		
-		
+        }
+
 		return puntos;
 	} 
 	
